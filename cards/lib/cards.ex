da75cd4f28a5a65e6005bd0644cd45a8f8f3d4a4
Khai Bui <khaivubui@gmail.com>
2017-10-26 10:04:02 -0700

    Write basic card functions

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,13 +1,26 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
-  def shuffle(deck) do
-    Enum.shuffle(deck)
+  def shuffle deck  do
+    Enum.shuffle deck
   end
 
-  def contains?(deck, card) do
-    Enum.member?(deck, card)
+  def contains? deck, card do
+    Enum.member? deck, card
+  end
+
+  def deal deck, hand_size do
+    Enum.split(deck, hand_size)
+  end
+
+  def save deck, filename do
+
   end
 end
