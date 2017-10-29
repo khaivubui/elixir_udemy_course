1c22e7e1a9486dffe6e8fcad97af35a80fdf3419
Khai Bui <khaivubui@gmail.com>
2017-10-29 10:31:10 -0700

    Build up build_grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -17,7 +17,9 @@ defmodule Identicon do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
-  def build_grid image do
-
+  def build_grid (%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    
   end
 end
