65b35ee4c42475e263e5c9614465e3a069015955
Khai Bui <khaivubui@gmail.com>
2017-10-28 20:07:38 -0700

    Touch up deal doctest and add case test

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -43,6 +43,12 @@ defmodule Cards do
       iex> {hand, deck} = Cards.deal(deck, 1)
       iex> hand
       ["Ace of Spades"]
+      iex> deck
+      ["Two of Spades", "Three of Spades", "Four of Spades", "Five of Spades",
+       "Ace of Clubs", "Two of Clubs", "Three of Clubs", "Four of Clubs",
+       "Five of Clubs", "Ace of Hearts", "Two of Hearts", "Three of Hearts",
+       "Four of Hearts", "Five of Hearts", "Ace of Diamonds", "Two of Diamonds",
+       "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
 
   """
   def deal deck, hand_size do
