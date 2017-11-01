60265fdaa6d29b905e91a3918d96737fca9f34c9
Khai Bui <khaivubui@gmail.com>
2017-10-31 22:35:10 -0700

    Add new route for topic

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -17,6 +17,7 @@ defmodule Discuss.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/topics", TopicController, only: [:new]
   end
 
   # Other scopes may use custom stacks.
