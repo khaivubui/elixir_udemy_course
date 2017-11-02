b90396d52ccec579cb2e88eeae04d4de720deecd
Khai Bui <khaivubui@gmail.com>
2017-11-01 17:27:41 -0700

    Change root route, add index route for topics

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -16,8 +16,10 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
-    resources "/topics", TopicController, only: [:new, :create]
+    get "/", TopicController, :index
+    resources "/topics", TopicController, only: [
+      :new, :create, :index
+    ]
   end
 
   # Other scopes may use custom stacks.
