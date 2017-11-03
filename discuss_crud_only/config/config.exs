fcc77c9ecad92ee6739b5d100d2efb005360ac66
Khai Bui <khaivubui@gmail.com>
2017-11-02 23:15:07 -0700

    Make a separate copy for crud only

---
+++ discuss_crud_only/config/config.exs
@@ -0,0 +1,28 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :discuss,
+  ecto_repos: [Discuss.Repo]
+
+# Configures the endpoint
+config :discuss, Discuss.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "mTJ/vSPQgWSkbwIR8UsBZOSI9UsTwmu0BY2+nQQu9j7oOV8dqnXUkBuKaWcAugj7",
+  render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Discuss.PubSub,
+           adapter: Phoenix.PubSub.PG2],
+  http: [protocol_options: [max_request_line_length: 8192, max_header_value_length: 8192]]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
---
+++ discuss_crud_only/web/controllers/topic_controller.ex
@@ -0,0 +1,55 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+  alias Discuss.Topic
+
+  def index conn, _params do
+    topics = Repo.all(from t in Topic, order_by: t.id)
+    render conn, :index, topics: topics
+  end
+
+  def new conn, _params do
+    changeset = Topic.changeset %Topic{}
+    render conn, :new, changeset: changeset
+  end
+
+  def create conn, %{"topic" => topic} do # topic == %{"title" => "abc"}
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, :new, changeset: changeset
+    end
+  end
+
+  def edit conn, %{"id" => topic_id} do
+    changeset = Repo.get(Topic, topic_id)
+      |> Topic.changeset
+    render conn, :edit, changeset: changeset
+  end
+
+  def update conn, %{"topic" => topic, "id" => topic_id} do
+    changeset = Repo.get(Topic, topic_id)
+      |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, :edit, changeset: changeset
+    end
+  end
+
+  def delete conn, %{"id" => topic_id} do
+    Repo.get(Topic, topic_id) |> Repo.delete
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: topic_path(conn, :index))
+  end
+end
