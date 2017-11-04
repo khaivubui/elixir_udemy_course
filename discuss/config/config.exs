d4617315b348187a9bf596c396f9c6bceefb22fa
Khai Bui <khaivubui@gmail.com>
2017-11-03 19:33:11 -0700

    Add config

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -26,3 +26,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.Oauth,
+  client_id: Secret.client_id,
+  client_secret: Secret.client_secret
