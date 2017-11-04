d20d1fa707320aa239a8bca280406e5b3e2fade8
Khai Bui <khaivubui@gmail.com>
2017-11-04 13:51:14 -0700

    Write the SetUser plug to set assign key to have user

--- discuss/web/controllers/plugs/set_user.ex
+++ discuss/web/controllers/plugs/set_user.ex
@@ -1,4 +1,20 @@
 defmodule Discuss.Plugs.SetUser do
   import Plug.Conn
   import Phoenix.Controller
+
+  alias Discuss.Repo
+  alias Discuss.User
+
+  def init(default), do: default
+
+  def call conn, _params do
+    user_id = get_session(conn, :user_id)
+
+    if user_id do
+      user = Repo.get(User, user_id)
+      assign conn, :user, user
+    else
+      assign conn, :user, nil
+    end
+  end
 end
