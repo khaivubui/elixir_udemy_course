e18d65abbeb3656a351082ae15989b937215d3f7
Khai Bui <khaivubui@gmail.com>
2017-11-04 00:21:47 -0700

    Build up callback action for auth

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -2,7 +2,17 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
+  alias Discuss.User
+
   def callback %{assigns: %{ueberauth_auth: auth}} = conn, params do
+    user_params = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      provider: params["provider"]
+    }
+
+    changeset = %User{} |> User.changeset(user_params)
+
     
   end
 end
