6f404c75bb5c9081ffa8f085e8b7e17c2855401e
Khai Bui <khaivubui@gmail.com>
2017-11-04 18:27:52 -0700

    Clean up code a little

--- discuss/web/controllers/plugs/set_user.ex
+++ discuss/web/controllers/plugs/set_user.ex
@@ -2,9 +2,6 @@ defmodule Discuss.Plugs.SetUser do
   import Plug.Conn
   import Phoenix.Controller
 
-  alias Discuss.Repo
-  alias Discuss.User
-
   def init _ do
   end
 
@@ -12,7 +9,7 @@ defmodule Discuss.Plugs.SetUser do
     user_id = get_session(conn, :user_id)
 
     if user_id do
-      user = Repo.get(User, user_id)
+      user = Discuss.Repo.get(Discuss.User, user_id)
       assign conn, :user, user
     else
       assign conn, :user, nil
