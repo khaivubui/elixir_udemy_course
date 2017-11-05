e4d7b19434faa886977009684003bfb55ffe2bbe
Khai Bui <khaivubui@gmail.com>
2017-11-05 12:48:37 -0800

    Add preloading comments to topic

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -4,7 +4,7 @@ defmodule Discuss.CommentsChannel do
 
   def join "comments:" <> topic_id, _params, socket do
     topic_id = String.to_integer(topic_id)
-    topic = Repo.get(Topic, topic_id)
+    topic = Repo.get(Topic, topic_id) |> Repo.preload(:comments)
 
     {:ok, %{}, assign(socket, :topic, topic)}
   end
