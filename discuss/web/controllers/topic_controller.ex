8bc51d47d61b2756b772d3d9a8b26e6422214c8d
Khai Bui <khaivubui@gmail.com>
2017-11-04 21:15:18 -0700

    Add plug to ensure its the topic owner

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -6,9 +6,17 @@ defmodule Discuss.TopicController do
     :new, :create, :edit, :update, :delete
   ]
 
+  plug :check_topic_owner when action in [
+    :update, :edit, :delete
+  ]
+
   def index conn, _params do
     topics = Repo.all(from t in Topic, order_by: t.id)
     render conn, :index, topics: topics
+  end
+
+  def show conn, %{"id" => topic_id} do
+    topic = Repo.get(Topic, topic_id)
   end
 
   def new conn, _params do
@@ -57,5 +65,18 @@ defmodule Discuss.TopicController do
     conn
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  def check_topic_owner conn, _params do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "Not authorized")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
   end
 end
