861e8dc41c612ddf5c078bfcac8efdf569357478
Khai Bui <khaivubui@gmail.com>
2017-11-05 20:17:36 -0800

    Add preload user per comment

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -4,16 +4,17 @@ defmodule Discuss.CommentsChannel do
 
   def join "comments:" <> topic_id, _params, socket do
     topic_id = String.to_integer(topic_id)
-    topic = Repo.get(Topic, topic_id) |> Repo.preload(:comments)
+    topic = Repo.get(Topic, topic_id) |> Repo.preload(comments: [:user])
 
     {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
   end
 
   def handle_in e_name, %{"content" => content}, socket do
     topic = socket.assigns.topic
+    user_id = socket.assigns.user_id
 
     changeset = topic
-      |> build_assoc(:comments, user_id: socket.assigns.user_id)
+      |> build_assoc(:comments, user_id: user_id)
       |> Comment.changeset(%{content: content})
 
     case Repo.insert changeset do
